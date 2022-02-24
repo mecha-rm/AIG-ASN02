784f79d09993c532228182ad5fcae7c105b23383
Roderick Montague <roderick.montague243@gmail.com>
2022-02-24 12:18:14 -0500

    Working on Audio Manager
    Working on the audio manager to add sound effects into the game.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// a manager for the game audio.
+public class AudioManager : MonoBehaviour
+{
+    // if 'true', the title manager is in the start function.
+    // this is used to stop sound effects from going off when the game starts.
+    private bool inStart = true;
+
+    // music
+    [Header("Background Music")]
+
+    // background music source. Only one bgm at a time is supported.
+    public AudioSource bgmSource;
+
+    // list of bgms
+    public List<AudioClip> bgmList;
+
+    // the sound effects
+    [Header("Sound Effects")]
+    public List<AudioSource> sfxList;
+
+    // if 'true', sound effects can play while the game is starting.
+    public bool allowSfxsOnStart = false;
+
+    // jingles and fanfares
+    [Header("Jingles/Fanfares")]
+    public List<AudioSource> jngList;
+
+    // if 'true', sound effects can play while the game is starting.
+    public bool allowJngsOnStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inStart = false;
+    }
+
+    // changes the background music.
+    public void ChangeBackgroundMusic(int index)
+    {
+        // no source set.
+        if (bgmSource == null)
+            return;
+
+        // index out of bounds.
+        if (index < 0 || index >= bgmList.Count)
+            return;
+
+        // new audio clip.
+        AudioClip newClip = bgmList[index];
+
+        bgmSource.Stop();
+        bgmSource.clip = newClip;
+        bgmSource.Play();
+    }
+
+    // plays a sound effect.
+    public void PlaySoundEffect(int index)
+    {
+        // in the start function, so play nothing.
+        if (inStart && !allowSfxsOnStart)
+            return;
+
+        // index out of bounds.
+        if (index < 0 || index >= sfxList.Count)
+            return;
+
+        // plays the audio.
+        sfxList[index].Play();
+    }
+
+    // plays a jingle.
+    public void PlayJingleEffect(int index)
+    {
+        // in the start function, so play nothing.
+        if (inStart && !allowJngsOnStart)
+            return;
+
+        // index out of bounds.
+        if (index < 0 || index >= jngList.Count)
+            return;
+
+        // plays the audio.
+        jngList[index].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
